e843e522d8514817451c6ffe9f77df8b0d8278f2
Your Name <you@example.com>
2022-10-07 23:14:03 +0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'Rimu', key: '1' },
+    { name: 'Zohra', key: '2' },
+    { name: 'Fatema', key: '3' },
+    { name: 'Fahad', key: '4' },
+    { name: 'Shohag', key: '5' },
+    { name: 'Rafi', key: '6' },
+    { name: 'Ashik', key: '7' },
+    { name: 'Tanisha', key: '8' },
+
+  ]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name} {item.key}</Text>
+
+        )}
+      />
     </View>
   );
 }
@@ -14,7 +33,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'hotpink',
+    fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
+  }
 });
